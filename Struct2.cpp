74bb985ba6da2676584ab954f7c5a82753939653
variannn340 <varianvarian0301@gmail.com>
2026-04-21 09:50:12 +0700

    Menambah int main Mahasiswa

---
+++ Struct2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat { //Address Detail
+    string desa; //village
+    string kota; //city
+};
+
+stric
--- struct1.cpp
+++ struct1.cpp
@@ -8,3 +8,21 @@ struct Mahasiswa {
     int umur;
     
 };
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.NIM;
+    cout << "\n nama : " << mhs.nama;
+    cout << "\n alamat : " << mhs.alamat;
+    cout << "\n umur : " << mhs.umur;
+
+}
