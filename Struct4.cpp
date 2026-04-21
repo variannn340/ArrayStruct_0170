3a96576924a46f5eb9a21f3c7629baa73be0d041
variannn340 <varianvarian0301@gmail.com>
2026-04-21 10:50:22 +0700

    Memperbaiki kesalahan space

--- Struct4.cpp
+++ Struct4.cpp
@@ -3,51 +3,50 @@
 
 using namespace std;
 
-
-struct DetailAlamat { //Address Detail
-    string desa; //village
-    string kota; //city
+struct DetailAlamat
+{                // Address Detail
+    string desa; // village
+    string kota; // city
 };
 
-struct Mahasiswa {
+struct Mahasiswa
+{
     string NIM;
     string nama;
-    DetailAlamat alamat; 
+    DetailAlamat alamat;
     int umur;
-    
 };
 
-int main() {
-    Mahasiswa mhs[3];
-    for (int i = 0; i < 3; i++) 
+int main()
 {
-    cout << "Data Mahasiswa Ke- " << (i+1) << ":" << endl;
-    cout << "NIM Mahasiswa : ";
-    getline(cin, mhs[i].NIM);
-    cout << "Nama Mahasiswa : ";
-    getline(cin, mhs[i].nama);
-    cout << "Alamat Mahasiswa : " << endl;
-    cout << "\t Nama Desa : ";
-    cin >> mhs[i].alamat.desa;
-    cout << "\t Nama Kota : ";
-    cin >> mhs[i].alamat.kota;
-    cout << "Umur Mahasiswa : ";
-    cin >> mhs[i].umur;
-}
+    Mahasiswa mhs[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Data Mahasiswa Ke- " << (i + 1) << ":" << endl;
+        cout << "NIM Mahasiswa : ";
+        getline(cin >> ws, mhs[i].NIM);
+        cout << "Nama Mahasiswa : ";
+        getline(cin >> ws, mhs[i].nama);
+        cout << "Alamat Mahasiswa : " << endl;
+        cout << "\t Nama Desa : ";
+        cin >> mhs[i].alamat.desa;
+        cout << "\t Nama Kota : ";
+        cin >> mhs[i].alamat.kota;
+        cout << "Umur Mahasiswa : ";
+        cin >> mhs[i].umur;
+    }
 
-for (int i = 0; i < 3; i++) {
-    cout << endl;
-    cout << "Data Mahasiswa Ke- " << (i+1) << ":" << endl;
-    cout << "\n NIM Mahasiswa : " << mhs[1].NIM;
-    cout << "\n Nama : " << mhs[1].nama;
-    cout << "\n Alamat : ";
-    cout << "\n Desa : " << mhs[1].alamat.desa;
-    cout << "\n Kota : " << mhs[1].alamat.kota;
-    cout << "\n umur : " << mhs[1].umur;
-}
+    for (int i = 0; i < 3; i++)
+    {
+        cout << endl;
+        cout << "Data Mahasiswa Ke- " << (i + 1) << ":" << endl;
+        cout << "\n NIM Mahasiswa : " << mhs[1].NIM;
+        cout << "\n Nama : " << mhs[1].nama;
+        cout << "\n Alamat : ";
+        cout << "\n Desa : " << mhs[1].alamat.desa;
+        cout << "\n Kota : " << mhs[1].alamat.kota;
+        cout << "\n umur : " << mhs[1].umur;
+    }
 
     return 0;
-
 }
-
-
