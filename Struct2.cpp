051fc46a8212757b14a879898b976b622cd615b5
variannn340 <varianvarian0301@gmail.com>
2026-04-21 10:01:38 +0700

    Tambah int main  Mahasiswa

--- Struct2.cpp
+++ Struct2.cpp
@@ -9,7 +9,32 @@ struct DetailAlamat { //Address Detail
 struct Mahasiswa {
     string NIM;
     string nama;
-    string DetailAlamat;
+    DetailAlamat alamat; 
     int umur;
     
 };
+
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\nNIM : " << mhs.NIM;
+    cout << "\nNama : " << mhs.nama;
+    cout << "\nAlamat : ";
+    cout << "\nDesa : " << mhs.alamat.desa;
+    cout << "\nKota : " << mhs.alamat.kota;
+    cout << "\numur : " << mhs.umur;
+
+}
